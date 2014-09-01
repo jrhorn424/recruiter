bb703b8bb66ec98468251ef29ea139d596d05e15
Pasha Podolsky <ppodolsky@me.com>
2014-09-01 10:30:48 +0400

    paper trail fix

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+Email.create([
+                 { id: 'confirm' },
+                 { id: 'reset', value: '## Your password has been reseted on the ICES Recruiter!
+[Come on](@url) to change your password.'},
+                 { id: 'deactivation' },
+                 { id: 'unlock' },
+                 { id: 'invite' }
+             ])
